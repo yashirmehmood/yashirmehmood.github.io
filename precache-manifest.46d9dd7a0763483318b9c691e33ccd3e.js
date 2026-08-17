d80c0d48ddef75e95852d967c79d526cc96f964c
Yashar Mahmood Lashkar <yashir.lashkar@gmail.com>
2026-08-17 21:59:53 +0200

    Updates

---
+++ precache-manifest.46d9dd7a0763483318b9c691e33ccd3e.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98d70ed1d9e4a7413decf308c545720",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b3f77ab8ea5e74e6937",
+    "url": "/static/css/main.b8cd9252.chunk.css"
+  },
+  {
+    "revision": "49cef2c5b63d23aab2ac",
+    "url": "/static/js/2.da3b06c1.chunk.js"
+  },
+  {
+    "revision": "3b3f77ab8ea5e74e6937",
+    "url": "/static/js/main.b3c6295a.chunk.js"
+  },
+  {
+    "revision": "a36a1dfbb9d4e404bf73",
+    "url": "/static/js/runtime-main.eca1abd0.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "74369f013539b7bc672b47c2d364abc9",
+    "url": "/static/media/Yashar_Mahmood_Lashkar_Resume.74369f01.pdf"
+  },
+  {
+    "revision": "56e6c0d613a48b47c8de5c0cabf496f5",
+    "url": "/static/media/abstron-logo.56e6c0d6.png"
+  },
+  {
+    "revision": "58b0a7d5801badddf2d097ce62f9b350",
+    "url": "/static/media/audi_logo.58b0a7d5.png"
+  },
+  {
+    "revision": "0bdb6072bb8d4c544e78068537917098",
+    "url": "/static/media/cisco_logo.0bdb6072.png"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "b55f7e3aec9df815a6ade01f51bfcb01",
+    "url": "/static/media/gaba_logo.b55f7e3a.png"
+  },
+  {
+    "revision": "d3bb6091548ff73f31fc6e0b6a5a14cb",
+    "url": "/static/media/hackmakers_logo.d3bb6091.jpg"
+  },
+  {
+    "revision": "6f1b28d856b15a343872c318dd6cec2d",
+    "url": "/static/media/icodeguru_logo.6f1b28d8.png"
+  },
+  {
+    "revision": "d5c792d3da1f521d75691135aec53b27",
+    "url": "/static/media/iiui_logo.d5c792d3.png"
+  },
+  {
+    "revision": "cff5f255f9cda6b6de22b1cdec140661",
+    "url": "/static/media/mercedes_logo.cff5f255.png"
+  },
+  {
+    "revision": "e7d97ba4e3cbcca6975d65370009fa9f",
+    "url": "/static/media/tu_chemnitz_logo.e7d97ba4.png"
+  },
+  {
+    "revision": "757fd218eea1c583a24838a2dfd35d5c",
+    "url": "/static/media/ultrahack_logo.757fd218.png"
+  },
+  {
+    "revision": "9b72503aed35b320b0f19f26e10f60ff",
+    "url": "/static/media/yashar_picture.9b72503a.png"
+  }
+]);
